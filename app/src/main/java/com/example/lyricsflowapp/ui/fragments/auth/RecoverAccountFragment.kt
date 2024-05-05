9a62119f2cd734d0deb2b78947be5bb7cae15088
pninci13 <p.ninci13@gmail.com>
2024-05-05 18:24:34 -0300

    add login or signup page and shapes for components

--- app/src/main/java/com/example/lyricsflowapp/ui/fragments/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/example/lyricsflowapp/ui/fragments/auth/RecoverAccountFragment.kt
@@ -1,4 +1,4 @@
-package com.example.lyricsflowapp.ui.fragments
+package com.example.lyricsflowapp.ui.fragments.auth
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.lyricsflowapp.R
 
-class LoginFragment : Fragment() {
+class RecoverAccountFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_recover_account, container, false)
     }
 
 }
