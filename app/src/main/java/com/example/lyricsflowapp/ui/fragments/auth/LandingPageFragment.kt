39315a8eff6e57f38f5432fcba40a2d00293c33c
pninci13 <p.ninci13@gmail.com>
2024-05-27 21:13:02 -0300

    add Login, Register and Recover Fragments and refactor strings to values

--- app/src/main/java/com/example/lyricsflowapp/ui/fragments/auth/LandingPageFragment.kt
+++ app/src/main/java/com/example/lyricsflowapp/ui/fragments/auth/LandingPageFragment.kt
@@ -7,14 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.lyricsflowapp.R
 
-class RegisterAccountFragment : Fragment() {
-
+class LandingPageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_account, container, false)
+        return inflater.inflate(R.layout.fragment_landing_page, container, false)
     }
 
 }
